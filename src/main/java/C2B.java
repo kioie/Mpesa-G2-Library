c5e75247bc320836f491ce81c40a2d6026e0dbec
kioi E <kioieddy@google.com>
2017-12-05 20:45:23 +0300

    Added a url registration methodk
    Signed-off-by: kioi E <kioieddy@google.com>

--- src/main/java/C2B.java
+++ src/main/java/C2B.java
@@ -8,8 +8,8 @@ import java.io.IOException;
  * Created by KioiEddy on 12/5/17.
  */
 public class C2B {
-    public JSONObject C2BSimulation(String consumer_key, String consumer_secret, String lipaNaMpesaShortcode,
-                                    String commandID, String amount, String phoneNumber) throws IOException {
+    public JSONObject Simulation(String consumer_key, String consumer_secret, String lipaNaMpesaShortcode,
+                                 String commandID, String amount, String phoneNumber) throws IOException {
 
 
         String url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate";
@@ -27,6 +27,29 @@ public class C2B {
 
         String requestJson = jsonArray.toString().replaceAll("[\\[\\]]", "");
         System.out.println(requestJson);
+        Authentication authenticator = new Authentication();
+
+        return authenticator.connect(consumer_key, consumer_secret, requestJson, url);
+    }
+
+    public JSONObject URLregistration(String consumer_key, String consumer_secret, String lipaNaMpesaShortcode,
+                                      String responseType,
+                                      String confirmationURL, String validationURL)
+            throws IOException {
+
+        String url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+        JSONArray jsonArray = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("ShortCode", lipaNaMpesaShortcode);
+        jsonObject.put("ResponseType", "Completed");
+        jsonObject.put("ConfirmationURL", confirmationURL);
+        jsonObject.put("ValidationURL", validationURL);
+        jsonArray.put(jsonObject);
+
+
+        String requestJson = jsonArray.toString().replaceAll("[\\[\\]]", "");
+        System.out.println(requestJson);
+
         Authentication authenticator = new Authentication();
 
         return authenticator.connect(consumer_key, consumer_secret, requestJson, url);
